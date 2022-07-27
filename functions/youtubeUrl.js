15856f1213d5c1f2ccd959760682bb6c7a066bf2
Miss Coder <misscoderexpert@gmail.com>
2022-07-27 09:40:59 +0400

    video-path

--- functions/youtubeUrl.js
+++ functions/youtubeUrl.js
@@ -1,26 +1,26 @@
 const ytdl = require('ytdl-core');
 const fs = require('fs');
-
+const path = require('path')
 function createVideo(url) {
 
-  
-  const videoName = `video.mp4`
+  // const videoName = `video.mp4`
+  const videoPath = path.join(__dirname, '../build/asd.mp4')
   ytdl(`${url}`)
-  .pipe(fs.createWriteStream(videoName));
+  .pipe(fs.createWriteStream(videoPath));
   
-  return videoName
+  return videoPath
 }
 
 exports.handler = async function(event, context) {
 
   if(event.body) {
       const {url} = JSON.parse(event.body)
-      const videoName = await createVideo(url)
-      console.log(videoName)
+      const videoPath = await createVideo(url)
+      console.log(videoPath)
      
-    // return {
-    //   statusCode:200,
-    //   body: JSON.stringify({url})
-    // }
+    return {
+      statusCode:200,
+      body: JSON.stringify({url})
+    }
   }
 }
