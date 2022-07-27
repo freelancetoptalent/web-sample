8449508f40f5b07aa995d26a33a9cdd907548916
Miss Coder <misscoderexpert@gmail.com>
2022-07-27 15:28:09 +0400

    update

--- functions/youtubeUrl.js
+++ functions/youtubeUrl.js
@@ -75,7 +75,7 @@ async function createVideo(videoURL) {
 
     // convert video to audio
     cp.execSync(`${ffmpeg} -loglevel 24 -i ${path.join(__dirname, `../build/xoski.mp4`)} -vn -sn -c:a mp3 -ab 192k  ${path.join(__dirname, `../build/xoski.mp3`)}`);
-    fs.rmSync(path.join(__dirname, `../build/xoski.mp4`));
+    // fs.rmSync(path.join(__dirname, `../build/xoski.mp4`));
 
 
     // id3.write(songTags, filePaths.audioFile);
